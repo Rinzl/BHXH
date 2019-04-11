9974b4e4f332e0b5e1742fc45e66511a9d882d70
dangth <tranhaidang2320@gmail.com>
2019-04-11 08:41:22 +0700

    Add findCityByCity

--- src/main/java/com/dangth/bhxh/repository/CityRepository.java
+++ src/main/java/com/dangth/bhxh/repository/CityRepository.java
@@ -4,4 +4,5 @@ import com.dangth.bhxh.model.location.City;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CityRepository extends CrudRepository<City, String> {
+    City findByCity(String name);
 }
