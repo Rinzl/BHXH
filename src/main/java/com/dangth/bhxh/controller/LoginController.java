082c4ff34d5001255200231501e6700913ccce23
dangth <tranhaidang2320@gmail.com>
2019-04-26 16:56:26 +0700

    Fix login page still accessible when user logged in

--- src/main/java/com/dangth/bhxh/controller/LoginController.java
+++ src/main/java/com/dangth/bhxh/controller/LoginController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.security.Principal;
+
 @Controller
 public class LoginController {
 
     @RequestMapping(value={"/login"}, method = RequestMethod.GET)
-    public ModelAndView login(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("login");
-        return modelAndView;
+    public String login(Principal principal){
+        return principal == null ? "/login" : "redirect:/";
     }
 
 
